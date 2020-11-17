5fb7f41a5b8de47cf639f1a70bd785328f25de8c
Trung Dang <dangtrung96@gmail.com>
2020-11-17 02:32:57 +0100

    fix __main__

--- crypto_return_predictor/__main__.py
+++ crypto_return_predictor/__main__.py
@@ -1,4 +1,2 @@
-from random_forest_crypto_return import random_forest_predictor
+from crypto_return_predictor import random_forest_predictor
 
-if __name__ == '__main__':
-    random_forest_predictor.run()
